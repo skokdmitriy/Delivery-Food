184589d63d4c736dab31227b7a58a936b1d16a4c
skokdmitriy <84864953+skokdmitriy@users.noreply.github.com>
2023-06-22 17:45:39 +0700

    Project structure Setup

---
+++ Delivery Food/Application/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Delivery Food
+//
+//  Created by Дмитрий Скок on 22.06.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = ViewController()
+        return true
+    }
+}
